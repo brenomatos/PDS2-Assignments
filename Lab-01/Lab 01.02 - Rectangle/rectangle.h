bee8f16864d32a13ba0f054908c681b9becc5f86
breno <breno.ms1997@gmail.com>
2018-09-08 13:41:47 -0300

    Added source code for lab1 assignments

---
+++ Lab-01/Lab 01.02 - Rectangle/rectangle.h
@@ -0,0 +1,9 @@
+#ifndef PDS2_VPL_RECT_H
+#define PDS2_VPL_RECT_H
+
+/* 
+ * Para suportar valores padrões. Crie 2 construtores, 1 sem parâmetros e outro
+ * com parâmetros.
+ */ 
+
+#endif
---
+++ Lab-01/Lab 01.03 - Aquecedor/aquecedor.h
@@ -0,0 +1,9 @@
+#ifndef PDS2_AQUECEDOR_H
+#define PDS2_AQUECEDOR_H
+
+// Nome dos metodos:
+// aquecer()
+// resfriar()
+// get_temperatura()
+
+#endif
